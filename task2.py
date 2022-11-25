bda5a88ec02c0546651558836380c59a7d56ef99
Сергей <sergemartin@yandex.ru>
2022-11-25 23:17:15 +0300

    решил task2.py

--- task2.py
+++ task2.py
@@ -1,2 +1,9 @@
 """2. Напишите программу для проверки ложности утверждения
 (W ⋀ Z) ⋁ ¬Y ⋁ (¬X ≡ ¬W) для всех значений предикат."""
+
+for x in range(2):
+    for y in range(2):
+        for w in range(2):
+            for z in range(2):
+                if(w and z) or not y or not (x == w):
+                    print(x, y, w, z)
