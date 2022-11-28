14da351f4e35995c56a1a434497e9a67a7370f1e
Сергей <sergemartin@yandex.ru>
2022-11-28 17:45:07 +0300

    внес коррективы к task2

--- task2.py
+++ task2.py
@@ -5,5 +5,5 @@ for x in range(2):
     for y in range(2):
         for w in range(2):
             for z in range(2):
-                if(w and z) or not y or not (x == w):
+                if not((w and z) or not y or  (not x ==(not w))):
                     print(x, y, w, z)
